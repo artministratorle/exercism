4342963cc63a2af37ea345fac0dbf87eb407820b
artministrator <artminstratorle@gmail.com>
2015-12-07 21:50:09 +0100

    hello Gänsefüsschen

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,11 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name=''):
+    if name=="":
+        name="Hello, World!"
+    else:
+        name="Hello, " + name + "!"
+    return name
